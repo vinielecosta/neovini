f59d44dfa832f5b927f79f53f501b0874f1fc7cd
Vinicius Eleuterio da Costa <Eleuteriodacostavinicius@gmail.com>
2025-08-14 22:03:28 -0300

    Update Neovim configuration enhance key mappings, improve DAP setup, and add toggleterm plugin

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,18 +1,27 @@
+-- ~/.config/nvim/lua/core/keymaps.lua
+-- Cria um atalho local para a função de mapeamento de teclas do Neovim
 local keymap = vim.keymap.set
 
--- Modo Normal
--- Navegação entre janelas
+-- MODO NORMAL (n)
+--------------------
+
+-- Salvar
+keymap('n', '<C-s>', ':w<CR>', {
+    desc = 'Salvar arquivo'
+})
+
+-- Navegação entre janelas (splits)
 keymap('n', '<C-h>', '<C-w>h', {
     desc = 'Mover para janela à esquerda'
-})
-keymap('n', '<C-l>', '<C-w>l', {
-    desc = 'Mover para janela à direita'
 })
 keymap('n', '<C-j>', '<C-w>j', {
     desc = 'Mover para janela abaixo'
 })
 keymap('n', '<C-k>', '<C-w>k', {
     desc = 'Mover para janela acima'
+})
+keymap('n', '<C-l>', '<C-w>l', {
+    desc = 'Mover para janela à direita'
 })
 
 -- Redimensionar janelas
@@ -29,56 +38,28 @@ keymap('n', '<C-Right>', ':vertical resize +2<CR>', {
     desc = 'Aumentar largura da janela'
 })
 
--- Fechar buffer
+-- Gerenciamento de Buffers
 keymap('n', '<leader>q', ':bdelete<CR>', {
     desc = 'Fechar buffer atual'
 })
 
--- Salvar
-keymap('n', '<C-s>', ':w<CR>', {
-    desc = 'Salvar arquivo'
-})
-
--- Modo de Inserção
--- Sair do modo de inserção rapidamente
-keymap('i', 'jk', '<ESC>', {
-    desc = 'Sair do modo de inserção'
-})
-
--- Modo Visual
--- Manter o texto selecionado após identar
-keymap('v', '<', '<gv', {
-    desc = 'Identar para a esquerda (manter seleção)'
-})
-keymap('v', '>', '>gv', {
-    desc = 'Identar para a direita (manter seleção)'
-})
-
--- NOVO ATALHO INTELIGENTE PARA RODAR PROJETOS .NET
+-- Rodar Projetos .NET
 keymap('n', '<leader>r', function()
     -- Usa o Telescope para encontrar apenas arquivos .csproj
     require('telescope.builtin').find_files({
         prompt_title = "Run .NET Project",
-        -- Comando para encontrar arquivos que terminam com .csproj na pasta atual
         find_command = {'fd', '--type', 'f', '--glob', '*.csproj'},
-        -- Mapeamento customizado para a seleção
         attach_mappings = function(prompt_bufnr, map)
-            -- Importa as funções de ação do Telescope
             local actions = require('telescope.actions')
             local action_state = require('telescope.actions.state')
 
-            -- Define uma nova ação a ser executada ao pressionar Enter
             local function run_dotnet_project()
-                -- Pega a entrada selecionada
                 local selection = action_state.get_selected_entry()
-                -- Fecha a janela do Telescope
                 actions.close(prompt_bufnr)
-                -- Monta e executa o comando no terminal
                 local command = 'split term://dotnet run --project ' .. selection.value
                 vim.cmd(command)
             end
 
-            -- Mapeia a tecla Enter para a nossa nova ação
             map('i', '<CR>', run_dotnet_project)
             map('n', '<CR>', run_dotnet_project)
             return true
@@ -87,3 +68,4 @@ keymap('n', '<leader>r', function()
 end, {
     desc = 'Rodar projeto .NET específico'
 })
+
